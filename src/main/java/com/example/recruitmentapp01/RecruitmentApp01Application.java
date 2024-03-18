4a5b8821c67a35a57fb6c622c34addab65384981
Iheb Cheikh <cheikhiheb@gmail.com>
2024-03-18 14:59:54 +0100

    Initial commit

---
+++ src/main/java/com/example/recruitmentapp01/RecruitmentApp01Application.java
@@ -0,0 +1,13 @@
+package com.example.recruitmentapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentApp01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentApp01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/recruitmentapp01/RecruitmentApp01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recruitmentapp01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentApp01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
